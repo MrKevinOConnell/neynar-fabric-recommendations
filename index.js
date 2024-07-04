a7a85c8e16f149f7ced7a23f3428eff75d3f594e
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-07-03 23:32:25 -0400

    dont need redis rn

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const redis = require("redis");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Initialize Redis client
-const redis_client = redis.createClient();
-
-redis_client.on("error", (err) => {
-  console.error("Redis error:", err);
-});
 
 // Middleware to parse JSON bodies
 app.use(express.json());
